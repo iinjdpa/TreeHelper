6df97f54ec45e3a9cd89365bd424d300989f9019
Daniel Pose <iinjdpa@users.noreply.github.com>
2021-11-29 12:41:20 +0100

    Solved problem for tree reconstruction
    I am using claimChildren method for tree reconstruction and RootObjects in combination with QtreeWidget.

--- treehelper.py
+++ treehelper.py
@@ -22,25 +22,56 @@ import FreeCAD as App
 import FreeCADGui as Gui
 from PySide import QtCore, QtGui
 from PySide.QtCore import Qt
-from PySide.QtGui import QLineEdit, QRadioButton, QDialog, QApplication, QStyleFactory, QTreeWidgetItem, QMainWindow
-from PySide.QtGui import QComboBox, QMessageBox, QFileDialog
+from PySide.QtGui import QDialog, QApplication, QTreeWidgetItem
+from PySide.QtGui import QMessageBox
 import os
 import sys
 from TreeHelper import ui_treewindow
 from collections import OrderedDict
 
-# import ptvsd
-# print("Waiting for debugger attach")
-# # 5678 is the default attach port in the VS Code debug configurations
-# ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
-# #ptvsd.wait_for_attach()
-# ptvsd.break_into_debugger()
-
 
 class Objdoc(object):
     def __init__(self,doc):
-        self.OutList = App.ActiveDocument.RootObjects
+        self.ViewObject = mViewObject()
         self.Name = doc.Name
+
+class mViewObject(object):
+    def __init__(self):
+        pass
+    def claimChildren(self):
+        # Nombre del documento
+        doc = App.activeDocument()
+        if doc == None:
+            doc = FreeCAD.newDocument()
+        docName = doc.Name
+        # Buscamos el árbol de FreeCAD. Es la forma de poder importar todos los objetos por orden
+        mw = Gui.getMainWindow()
+        self.tw = mw.findChildren(QtGui.QTreeWidget)
+        self.tw0 = self.tw[0]
+        self.FCtreeitems = self.tw0.findItems('*', Qt.MatchWrap | Qt.MatchWildcard  | Qt.MatchRecursive)
+        root_labels = []
+        for it in self.FCtreeitems:
+            if not it.parent(): continue
+            if isinstance(it.parent(), list):
+                for itp in it.parent():
+                    if itp == docName:
+                        root_labels.append(it.text(0))
+            else:
+                if it.parent().text(0) == docName:
+                    root_labels.append(it.text(0))
+        root_objects = App.ActiveDocument.RootObjects
+        root_objects_from_labels = []
+        if len(root_labels) == len(root_objects):
+            return root_objects
+        else:
+            for obj in root_labels:
+                obj_by_label = App.ActiveDocument.getObjectsByLabel(obj)
+                if len(obj_by_label) > 1:
+                    QMessageBox.critical(self, "Error","There are objects with same label!")
+                    return []
+                else:
+                    root_objects_from_labels.append(App.ActiveDocument.getObjectsByLabel(obj)[0])
+        return root_objects_from_labels
 
 class TreeDrops(QtGui.QTreeWidget):
     itemDropped = QtCore.Signal()
@@ -106,12 +137,6 @@ class Treehelper(QDialog,ui_treewindow.Ui_Dialog):
         self.mainelement = ''
         # Hacemos la ventana modal
         # self.setModal(False)
-        # Buscamos el árbol de FreeCAD. Es la forma de poder importar todos los objetos por orden
-        mw = Gui.getMainWindow()
-        self.tw = mw.findChildren(QtGui.QTreeWidget)
-        self.tw0 = self.tw[0]
-        self.FCtreeitems = self.tw0.findItems('*', Qt.MatchWrap | Qt.MatchWildcard  | Qt.MatchRecursive)[0]
-
         # Cargamos el árbol
         self.loadtree()
 
@@ -120,57 +145,6 @@ class Treehelper(QDialog,ui_treewindow.Ui_Dialog):
 
     def updatetree(self):
         self.loadtree()
-
-    def old_loadtree(self):
-        """Lee el árbol de FreeCAD a partir del elemento seleccionado y lo carga en el árbol de ayuda
-        """
-        self.treeWidget2.clear()
-
-        # El objeto seleccionado será el primero del árbol
-        if Gui.Selection.getCompleteSelection():
-            if not self.mainelement:
-                objSel = Gui.Selection.getCompleteSelection()[0]
-                self.mainelement = objSel
-            else:
-                objSel = self.mainelement
-            mainTreeItem  = QTreeWidgetItem(self.treeWidget2)
-            mainTreeItem.setText(0,objSel.Label)
-            mainTreeItem.setText(1,objSel.Name)
-            mainTreeItem.setIcon(0,objSel.ViewObject.Icon)
-            mainTreeItem.setExpanded(True)
-        else:
-            doc = App.activeDocument()
-            if doc == None:
-                doc = FreeCAD.newDocument()
-            mainTreeItem  = QTreeWidgetItem(self.treeWidget2)
-            mainTreeItem.setText(0,doc.Name)
-            mainTreeItem.setText(1,doc.Name)
-            mainTreeItem.setExpanded(True)
-            objSel = Objdoc(doc)
-
-        # Iteramos sobre los objetos bajo el objeto seleccionado
-        dictObjs = OrderedDict()
-        lstObjs = [[0,objSel,'main']]
-        while lstObjs:
-            crrObj = lstObjs.pop()
-            for obj in crrObj[1].OutList:
-                level = crrObj[0]
-                # Guardamos su nivel dentro del árbol, el propio objeto y el nombre del objeto padre
-                dictObjs[obj.Name] = [level+1,obj,crrObj[1].Name]
-                lstObjs.append(dictObjs[obj.Name])
-        
-        # Iteramos ahora sobre los objetos recogidos para insertarlos en el árbol
-        dictItems = {}
-        dictItems[objSel.Name] = mainTreeItem
-        # print('El contenido de dictObjs: ',dictObjs)
-        # print('El contenido de dictItems: ',dictItems)
-        for k,elem in dictObjs.items():
-            itemElem = QTreeWidgetItem(dictItems[dictObjs[k][2]])
-            itemElem.setText(0,dictObjs[k][1].Label)
-            itemElem.setText(1,dictObjs[k][1].Name)
-            itemElem.setIcon(0,dictObjs[k][1].ViewObject.Icon)
-            itemElem.setExpanded(True)
-            dictItems[k] = itemElem
 
     def loadtree(self):
         """Lee el árbol de FreeCAD a partir del elemento seleccionado y lo carga en el árbol de ayuda
@@ -204,7 +178,7 @@ class Treehelper(QDialog,ui_treewindow.Ui_Dialog):
         lstObjs = [[0,objSel,'main']]
         while lstObjs:
             crrObj = lstObjs.pop()
-            for obj in crrObj[1].OutList:
+            for obj in crrObj[1].ViewObject.claimChildren():
                 level = crrObj[0]
                 # Guardamos su nivel dentro del árbol, el propio objeto y el nombre del objeto padre
                 dictObjs[obj.Name] = [level+1,obj,crrObj[1].Name]
@@ -213,8 +187,6 @@ class Treehelper(QDialog,ui_treewindow.Ui_Dialog):
         # Iteramos ahora sobre los objetos recogidos para insertarlos en el árbol
         dictItems = {}
         dictItems[objSel.Name] = mainTreeItem
-        print('El contenido de dictObjs: ',dictObjs)
-        print('El contenido de dictItems: ',dictItems)
         for k,elem in dictObjs.items():
             itemElem = QTreeWidgetItem(dictItems[dictObjs[k][2]])
             itemElem.setText(0,dictObjs[k][1].Label)
